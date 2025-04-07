55766ce38076ddccc78589bbc0814f9bf1d73367
Ian Cullinane <ian.cullinane@gmail.com>
2025-04-07 15:47:54 -0400

    testing todo again

--- scenes/map/map.gd
+++ scenes/map/map.gd
@@ -26,8 +26,7 @@ func get_terrain_layer()->TileMapLayer:
 	#var tile_size = tilemap_layer.get_tile_map().tile_set.tile_size
 	#draw_rect(Rect2(top_left, tile_size), Color.RED, false)
 
-# TODO the entities are too tightly coupled to the map
-# TODO: Come up with a more imaginative greeting
+# TODO: the entities are too tightly coupled to the map
 func update_ui_from_pos(map: GameMap, ui):
 	var ground_layer: TileMapLayer = map.get_ground_layer()
 	var terrain_layer: TileMapLayer = map.get_terrain_layer()
