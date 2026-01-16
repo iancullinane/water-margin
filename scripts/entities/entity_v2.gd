e2bb70438b90294594694472a0a9cd842e348e1c
Ian Cullinane <ian.cullinane@gmail.com>
2026-01-16 14:43:39 -0500

    entity_v2 in game with knight animation

--- scripts/entities/entity_v2.gd
+++ scripts/entities/entity_v2.gd
@@ -9,3 +9,87 @@ var CELL_SIZE = GameConstants.CELL_SIZE
 
 @onready var sprite = $AnimatedSprite2D
 @onready var name_label = $NameLabel
+@onready var animation_player = $AnimationPlayer
+
+
+func _ready() -> void:
+	animation_player.play("idle_down")
+
+
+# Player Movement
+#
+# Basic Movement
+# ==============
+
+func _on_move(direction):
+	_move(direction)
+
+# This is no longer a mobile game at all, remove touch
+# func _on_swipe(direction):
+# 	_move(direction)
+
+func _move(dir: String) -> void:
+	var target_pos := position
+
+	# Movement is based on the size of cells
+	if dir == "up":
+		target_pos.y -= CELL_SIZE
+	elif dir == "down":
+		target_pos.y += CELL_SIZE
+	elif dir == "left":
+		target_pos.x -= CELL_SIZE
+	elif dir == "right":
+		target_pos.x += CELL_SIZE
+
+
+	# if not _can_move_to(target_pos):
+	# 	return
+
+	if dir == "up":
+		sprite.animation = "up_idle"
+	elif dir == "down":
+		sprite.animation = "down_idle"
+	elif dir == "left":
+		sprite.animation = "left_idle"
+	elif dir == "right":
+		sprite.animation = "right_idle"
+
+	position = target_pos
+
+	# if current_player:
+	# 	SignalBus.current_player_moved.emit(self)
+
+# func _can_move_to(target_pos: Vector2) -> bool:
+# 	var game_map := _get_current_map()
+# 	if game_map == null:
+# 		return true
+
+# 	var tile_coords := game_map.get_tile_from_global(target_pos)
+
+# 	if _is_tile_blocked(game_map, tile_coords):
+# 		return false
+
+# 	if _is_entity_at_position(target_pos):
+# 		return false
+
+# 	return true
+
+
+# Zoom
+# TODO: Implement
+# ====
+
+# func _on_zoom(direction):
+# 	if direction == "in":
+# 		zoom_in()
+# 	elif direction == "out":
+# 		zoom_out()
+
+
+# func _input(event):
+
+# 	if event.is_action("zoom_in"):
+# 		emit_signal("zoomed", "in")
+
+# 	if event.is_action("zoom_out"):
+# 		emit_signal("zoomed", "out")
